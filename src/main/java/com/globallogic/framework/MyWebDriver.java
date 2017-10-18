731ff4c2bb9735d7d152cc8be8fe8b56bf8ef5de
sandro-ua <qatestermailbox@gmail.com>
2017-10-18 05:43:15 -0700

    fixing Chrome issue #1

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -22,7 +22,7 @@ public class MyWebDriver {
         public static void Initialize() {
             System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
             ChromeOptions chromeOptions = new ChromeOptions();
-
+            chromeOptions.setBinary("C:\\Users\\mylet\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
             Instance = new ChromeDriver(chromeOptions);
             Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
