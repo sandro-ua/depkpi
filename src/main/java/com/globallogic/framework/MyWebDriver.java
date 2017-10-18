84361c728bd280c1c93201f3cf79c82eac2d1b1c
sandro-ua <qatestermailbox@gmail.com>
2017-10-18 05:37:36 -0700

    switching to Chrome

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -20,18 +20,15 @@ public class MyWebDriver {
         }
 
         public static void Initialize() {
-
-
-/*          System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
             ChromeOptions chromeOptions = new ChromeOptions();
 
             Instance = new ChromeDriver(chromeOptions);
-            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);*/
-            System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
-            Instance = new FirefoxDriver();
             Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-
+/*          System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
+            Instance = new FirefoxDriver();
+            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);*/
 
         }
 
