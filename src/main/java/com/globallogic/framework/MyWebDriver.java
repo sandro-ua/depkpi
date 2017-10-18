4c2e57f4660ec8d8802837116c132a72668512a1
sandro-ua <qatestermailbox@gmail.com>
2017-10-18 03:23:46 -0700

    added chrome options added path to ChromeDriver

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -2,6 +2,8 @@ package com.globallogic.framework;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.concurrent.TimeUnit;
 
 public class MyWebDriver {
@@ -17,7 +19,11 @@ public class MyWebDriver {
         }
 
         public static void Initialize() {
-            Instance = new ChromeDriver();
+
+            System.setProperty("webdriver.chrome.driver", "/resources/chromedriver.exe");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            Instance = new ChromeDriver(chromeOptions);
             Instance.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         }
     }
