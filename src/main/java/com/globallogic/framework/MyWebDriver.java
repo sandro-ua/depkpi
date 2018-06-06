7cb153a56454bfdc62f1b9c266e1e7db948ca7b7
sandro-ua <qatestermailbox@gmail.com>
2018-06-06 09:07:04 -0400

    added Driver Manager

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -1,5 +1,6 @@
 package com.globallogic.framework;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,29 +11,27 @@ import java.util.concurrent.TimeUnit;
 public class MyWebDriver {
 
     public static WebDriver Instance;
-
     public static WebDriver getInstance() {
             return Instance;
         }
-
-        public static void setInstance(WebDriver instance) {
+    public static void setInstance(WebDriver instance) {
             Instance = instance;
         }
 
         public static void Initialize() {
 
-        /*
-            System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
+            WebDriverManager.chromedriver().setup();
+
+            //System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.setBinary("C:\\Users\\mylet\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+            //chromeOptions.setBinary("C:\\Users\\mylet\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
             Instance = new ChromeDriver(chromeOptions);
             Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-*/
-         System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
-            Instance = new FirefoxDriver();
-            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
-        }
+/*         System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
+            Instance = new FirefoxDriver();
+            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);*/
+       }
 
 
     }
