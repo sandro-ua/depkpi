cf69600f080aa9fc2b3a3a20d465331a6d0f42a8
sandro-ua <qatestermailbox@gmail.com>
2018-06-06 10:09:48 -0400

    added Driver Manager - fix

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,9 +24,9 @@ public class MyWebDriver {
             WebDriverManager.chromedriver().setup();
 
             //System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
-            ChromeOptions chromeOptions = new ChromeOptions();
+            //ChromeOptions chromeOptions = new ChromeOptions();
             //chromeOptions.setBinary("C:\\Users\\mylet\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
-            Instance = new ChromeDriver(chromeOptions);
+            Instance = new ChromeDriver();
             Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 /*         System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
