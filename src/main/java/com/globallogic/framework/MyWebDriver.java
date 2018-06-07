6ecad2a117b0bbbd34ee81fc7f19bd6eb9f6d384
sandro-ua <qatestermailbox@gmail.com>
2018-06-07 08:02:36 -0400

    OS selection

--- src/main/java/com/globallogic/framework/MyWebDriver.java
+++ src/main/java/com/globallogic/framework/MyWebDriver.java
@@ -18,6 +18,7 @@ public class MyWebDriver {
     public static void setInstance(WebDriver instance) {
             Instance = instance;
         }
+    private static String OS = System.getProperty("os.name").toLowerCase();
 
         public static void Initialize() {
 
@@ -27,14 +28,27 @@ public class MyWebDriver {
             //ChromeOptions chromeOptions = new ChromeOptions();
             //chromeOptions.setBinary("C:\\Users\\mylet\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
 
+            if (isUnix()) {
+                Instance = new ChromeDriver();
+                Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            }
+            if (isWindows()) {
+                //System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
+                Instance = new FirefoxDriver();
+                Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            }
+        }
 
-/*            Instance = new ChromeDriver();
-            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);*/
+    public static boolean isUnix() {
 
-            //System.setProperty("webdriver.gecko.driver", "src/resources/geckodriver.exe");
-            Instance = new FirefoxDriver();
-            Instance.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-       }
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
 
+    }
+
+    public static boolean isWindows() {
+
+        return (OS.indexOf("win") >= 0);
+
+    }
 
     }
